f0709ce6a13965edc9a3acf2f2627a6dc4e480ee
antonioplacerda <antonioplacerda@gmail.com>
2021-07-15 03:57:18 +0100

    [frontend] finish frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,66 @@
-import web3 from './web3'
+import { useState, useEffect } from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
 
-const App = () => <p>{web3.version}</p>
+const App = () => {
+  const [owner, setOwner] = useState("");
+  const [numberOfPlayers, setNumberOfPlayers] = useState(0);
+  const [totalAmount, setTotalAmount] = useState("");
+  const [inTransaction, setInTransaction] = useState(false);
+
+  useEffect(() => {
+    lottery.methods.owner().call().then(setOwner);
+    lottery.methods.getNumberOfPlayers().call().then(setNumberOfPlayers);
+    web3.eth
+      .getBalance(lottery.options.address)
+      .then((wei) => setTotalAmount(web3.utils.fromWei(wei, "ether")));
+  }, []);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    setInTransaction(true);
+
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei("0.001"),
+      });
+    } catch (err) {}
+
+    setInTransaction(false);
+  };
+
+  const onClick = async (e) => {
+    const accounts = await web3.eth.getAccounts();
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+  };
+
+  return (
+    <>
+      <h1>Welcome to the lottery!</h1>
+      <p>This lottery is owned by: {owner}</p>
+      <br />
+      <p>
+        Currently, there are {numberOfPlayers} in the race, for a whooping sum
+        of {totalAmount} ether!
+      </p>
+      <hr />
+      {inTransaction && <p>Waiting for transaction to finish...</p>}
+      <form onSubmit={onSubmit}>
+        <h4>Want to enter?</h4>
+        <button>Enter with 0.001 ether</button>
+      </form>
+      <hr />
+      <h4>Let's pick a winner!</h4>
+      <button onClick={onClick}>Pick a winner</button>
+    </>
+  );
+};
 
 export default App;
--- frontend/src/lottery.js
+++ frontend/src/lottery.js
@@ -1,7 +1,72 @@
 import web3 from "./web3";
 
-const address = '0xEE7F2cD64543fe47BE2cae452510131b829D60fd'
+const address = "0xEE7F2cD64543fe47BE2cae452510131b829D60fd";
 
-const { abi } = require("../contracts/solidity/Lottery.json");
+const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getNumberOfPlayers",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "players",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
 
 export default new web3.eth.Contract(abi, address);
